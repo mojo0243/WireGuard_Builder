b2a75ca38863d57d79abf1e67f8c6b4b23e309f3
root <root@localhost.localdomain>
2020-04-20 13:17:02 -0400

    Fixed issue deleted wg0.conf file automatically and added option to add peer to file

--- build_wg.py
+++ build_wg.py
@@ -11,13 +11,14 @@ from time import sleep
 
 wg_parser = argparse.ArgumentParser()
 
-wg_parser.add_argument('-p', dest='port', action='store', required=True, help='Specify the port for the server')
+wg_parser.add_argument('-p', dest='port', action='store', help='Specify the port for the server')
 wg_parser.add_argument('-c', '--client', dest='client', action='store_true', help='Use this to create a client config')
 wg_parser.add_argument('-s', '--server', dest='server', action='store_true', help='Use this to create a client config')
-wg_parser.add_argument('-i', dest='ip', action='store', required=True, help='Place the ip you want the server to use Ex: 10.0.0.1/24')
+wg_parser.add_argument('-i', dest='ip', action='store', help='Place the ip you want the server to use Ex: 10.0.0.1/24')
 wg_parser.add_argument('-k', '--key', action='store', dest='key', help='Place the public key for the server here')
 wg_parser.add_argument('-e', '--endpoint', action='store', dest='endpoint', help='Place ther server IP here')
 wg_parser.add_argument('-a', '--allow', action='store', dest='allow', help='Place the allowed IPs for communiction here Ex: 10.0.0.0/24')
+wg_parser.add_argument('-r', '--peer', dest='peer', action='store_true', help='Use this to add a peer to the client config')
 
 if len(sys.argv) == 1:
     wg_parser.print_help(sys.stderr)
@@ -33,15 +34,14 @@ init(autoreset=True)
 
 wg0 = "/etc/wireguard/wg0.conf"
 
-if os.path.isfile(wg0):
-    os.remove(wg0)
-
 # Build the correct wireguard config file
 
 if wg.client and wg.server:
     print(Fore.RED+Style.BRIGHT+"You must select to build only a client or a server")
 elif wg.client:
-    if wg.key and wg.endpoint and wg.allow:
+    if wg.key and wg.endpoint and wg.allow and wg.ip and wg.port:
+        if os.path.isfile(wg0):
+            os.remove(wg0)
         print("")
         print(Fore.YELLOW+Style.BRIGHT+"Building Client config now")
         os.system("umask 077")
@@ -60,24 +60,40 @@ elif wg.client:
         os.system("echo AllowedIPs = {allow} >> {wg0}".format(allow=wg.allow, wg0=wg0))
         print(Fore.GREEN+"Server config complete")
     else:
-        print(Fore.RED+Style.BRIGHT+"Building a client config file requires a -k <public key>, -e <server ip>, and -a <allowed IPS>")
+        print(Fore.RED+Style.BRIGHT+"Building a client config file requires a -i <ip/cidr> -p <port> -k <public key>, -e <server ip>, and -a <allowed IPS>")
 elif wg.server:
-    print("")
-    print(Fore.YELLOW+Style.BRIGHT+"Building Server config now")
-    os.system("umask 077")
-    os.system("wg genkey | tee privatekey | wg pubkey > publickey")
-    os.system("echo [Interface] >> {wg0}".format(wg0=wg0))
-    sleep(1)
-    with open("privatekey") as p:
-        priv = p.readline()
-        priv = priv.strip("\n")
-    p.close
-    os.system("echo 'PrivateKey = {priv}' >> {wg0}".format(priv=priv, wg0=wg0))
-    os.system("echo Address = {addr} >> {wg0}".format(addr=wg.ip, wg0=wg0))
-    os.system("echo Listenport = {port} >> {wg0}".format(port=wg.port, wg0=wg0))
-    os.system("echo 'PostUp = iptables -A FORWARD -i wg0 -j ACCEPT; iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE; iptables -I INPUT -p tcp --dport {port} -j ACCEPT; sysctl -w net.ipv4.ip_forward=1' >> {wg0}".format(wg0=wg0, port=wg.port))
-    os.system("echo 'PostDown = iptables -D FORWARD -i wg0 -j ACCEPT; iptables -t nat -D POSTROUTING -o eth0 -j MASQUERADE; iptables -D INPUT -p tcp --dport {port} -j ACCEPT; sysctl -w net.ipv4.ip_forward=0' >> {wg0}".format(wg0=wg0, port=wg.port))
-    os.system("echo SaveConfig = true >> {wg0}".format(wg0=wg0))
-    print(Fore.GREEN+"Server config complete")
+    if wg.port and wg.ip:
+        if os.isfile(wg0):
+            os.remove(wg0)
+        print("")
+        print(Fore.YELLOW+Style.BRIGHT+"Building Server config now")
+        os.system("umask 077")
+        os.system("wg genkey | tee privatekey | wg pubkey > publickey")
+        os.system("echo [Interface] >> {wg0}".format(wg0=wg0))
+        sleep(1)
+        with open("privatekey") as p:
+            priv = p.readline()
+            priv = priv.strip("\n")
+        p.close
+        os.system("echo 'PrivateKey = {priv}' >> {wg0}".format(priv=priv, wg0=wg0))
+        os.system("echo Address = {addr} >> {wg0}".format(addr=wg.ip, wg0=wg0))
+        os.system("echo Listenport = {port} >> {wg0}".format(port=wg.port, wg0=wg0))
+        os.system("echo 'PostUp = iptables -A FORWARD -i wg0 -j ACCEPT; iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE; iptables -I INPUT -p udp --dport {port} -j ACCEPT; sysctl -w net.ipv4.ip_forward=1' >> {wg0}".format(wg0=wg0, port=wg.port))
+        os.system("echo 'PostDown = iptables -D FORWARD -i wg0 -j ACCEPT; iptables -t nat -D POSTROUTING -o eth0 -j MASQUERADE; iptables -D INPUT -p udp --dport {port} -j ACCEPT; sysctl -w net.ipv4.ip_forward=0' >> {wg0}".format(wg0=wg0, port=wg.port))
+        os.system("echo SaveConfig = true >> {wg0}".format(wg0=wg0))
+        print(Fore.GREEN+"Server config complete")
+    else:
+        print(Fore.RED+Style.BRIGHT+"Building a server config requires a -i <ip/cidr>, -p <port>")
+elif wg.peer:
+    if wg.key and wg.allow:
+        print("")
+        print(Fore.YELLOW+Style.BRIGHT+"Adding Peer to configuration file")
+        os.system("echo '' >> {wg0}".format(wg0=wg0))
+        os.system("echo '[Peer]' >> {wg0}".format(wg0=wg0))
+        os.system("echo PublikKey = {key} >> {wg0}".format(key=wg.key, wg0=wg0))
+        os.system("echo AllowedIPs = {allow} >> {wg0}".format(allow=wg.allow, wg0=wg0))
+        print(Fore.GREEN+"Peer added to configuration file")
+    else:
+        print(Fore.RED+Style.BRIGHT+"Adding a Peer to the configuration requires a -k <public key> and -a <allowed IPS>")
 else:
-    print("You must select either a -c for client or -s for server")
+    print("You must select either a -c for client, -s for server or -r for peer")
